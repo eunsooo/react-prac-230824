c8ccdc5a5ed01ee6fe6f30eeb52029dd5887c7eb
eunsooo <eunxoolee@gmail.com>
2023-08-25 14:38:24 +0900

    routing, component, sass setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeView from "./views/HomeView";
+import AboutView from "./views/AboutView";
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <HomeView /> } />  {/* 메인 페이지 생성 */ }
+                <Route path="/about" element={ <AboutView /> } /> {/* about 페이지 생성 */ }
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
