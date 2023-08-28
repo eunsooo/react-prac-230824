b239b96f543e5719500ca1bbb565154bda16567c
eunsooo <eunxoolee@gmail.com>
2023-08-28 20:16:19 +0900

    contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React from "react"
+
+const contactText = [
+  {
+      link: "https://velog.io/@eunsooo",
+      title: "blog : eunsooo.log"
+  },
+  {
+      link: "mailto:eunxoolee@gmail.com",
+      title: "mail : eunxoolee@gmail.com"
+  }
+];
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section id="contact">
+      <div className="contact__inner">
+        <h2 className="contact__title">Contact</h2>
+        <div className="contact__lines top" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="contact__text">
+          <div className="text">
+            {contactText.map((contact, key) => (
+              <div key={key}>
+                <a 
+                  href={contact.link} 
+                  rel="noopenner noreferrer" 
+                  target="_blank"
+                >
+                  {contact.title}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="contact__lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
   )
 }
 
